9feb746c618613e15e6f4e1feb85c9507938dea5
MUSTAKIMBIN <mustakimbin24@gmail.com>
2025-11-10 14:24:59 +0600

    added navber and footer

--- src/Pages/Root/Root.jsx
+++ src/Pages/Root/Root.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router";
+import Navber from "../../Components/Navber/Navber";
+import Footer from "../../Components/Footer/Footer";
 
 const Root = () => {
   return (
     <div>
-      root Page
+      <Navber></Navber>
       <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   );
 };
